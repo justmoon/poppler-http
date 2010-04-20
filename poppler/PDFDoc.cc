f11123d1b4c38a7a254ec2fbe41a0b96c85a88fc
Hib Eris <hib@hiberis.nl>
2010-04-20 19:03:54 +0200

    add PDFDoc::getPage()

--- poppler/PDFDoc.cc
+++ poppler/PDFDoc.cc
@@ -1038,3 +1038,11 @@ Guint PDFDoc::getMainXRefEntriesOffset()
   return mainXRefEntriesOffset;
 }
 
+Page *PDFDoc::getPage(int page)
+{
+  if ((page < 1) || page > getNumPages()) return NULL;
+
+  {
+    return catalog->getPage(page);
+  }
+}
